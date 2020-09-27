086f640bfe93dccdfb203cd1d0d269618dcbced1
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-27 21:00:18 +0100

    getting post requests from postman to database

--- routes/routes.js
+++ routes/routes.js
@@ -2,18 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-
 // routes
 
-router.get('/', ( req, res ) => {
-  res.send('This is an example of basic rest api...')
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({message: err});
+  }
 });
 
-router.post('/', (req,res) => {
-  console.log(req.body);
-})
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-
-
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 module.exports = router;
