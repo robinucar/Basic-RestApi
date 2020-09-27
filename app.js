cea7ca5451832acf0c36679a25d0971e1e459c79
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-27 18:33:35 +0100

    second route for home page

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', ( req, res ) => {
   res.send('This is an example of basic rest api...')
 })
 
-// listening
+app.get('/home', ( req, res ) => {
+  res.send('We are on home...')
+})
+
+// lisitening to the server
 
 app.listen(3000)
