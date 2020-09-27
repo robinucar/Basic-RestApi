83f2009bbc86fab23a34840243539616347ac151
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-27 18:29:33 +0100

    first route and listening

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const app = express();
+
+// Routes
+
+app.get('/', ( req, res ) => {
+  res.send('This is an example of basic rest api...')
+})
+
+// listening
+
+app.listen(3000)
